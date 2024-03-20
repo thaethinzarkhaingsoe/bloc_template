9593b0a2ffab2b57fca7a1a70909da2d8807d4ae
ThaeThinzar <=thae.thinzar.khaing.soe@gmail.com>
2024-03-20 19:06:51 +0900

    update folder structure

---
+++ lib/src/home/presentations/view/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
